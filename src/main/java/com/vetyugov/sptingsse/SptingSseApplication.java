c7f9ee903f49ee4cd396035abf0a03c7e5e145e0
Vetyugov <nickitavet1998@gmail.com>
2022-12-26 12:25:35 +0300

    Initial settings

---
+++ src/main/java/com/vetyugov/sptingsse/SptingSseApplication.java
@@ -0,0 +1,13 @@
+package com.vetyugov.sptingsse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptingSseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptingSseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vetyugov/sptingsse/SptingSseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetyugov.sptingsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptingSseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
